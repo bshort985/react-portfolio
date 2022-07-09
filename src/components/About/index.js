80060fb7f4390e43a4155b9bbb4c10084d1fe5d2
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-09 15:45:19 -0400

    nav functionaality added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,12 @@
+// About page
+
 import React from "react";
 
 function About() {
     return (
-        <section>
+        <section className="my-5">
             <h1 id="about">About Me</h1>
+            <p>Add text here: Who am i? Background. Current Postion. Why I want to change careers. What I want from the future. What my motovations are.</p>
         </section>
     );
 }
---
+++ src/components/Page/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PageContent from '../PageContent';
+import About from '../About';
+// import Portfolio from '../Portfolio';
+// import Contact from '../Contact';
+// import Resume from '../Resume';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Page({ currentPage }) {
+
+  const renderPage = () => {
+    switch (currentPage.name) {
+      case 'about me':
+        return <About />;
+      default:
+        return <About />;
+    }
+  };
+
+  return (
+    <section>
+      <h2>{capitalizeFirstLetter(currentPage.name)}</h2>
+      <PageContent>{renderPage()}</PageContent>
+    </section>
+  );
+}
+export default Page;
